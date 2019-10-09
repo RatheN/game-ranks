2d8b56fda335d7553cf2a4e2bc382608ea9b1e57
Nate <nprathe10@gmail.com>
2019-10-09 17:50:31 -0500

    setup app_controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+
+    get '/games' do 
+        if logged_in?
+            @user = current_user
+            @posts = current_user.games
+            
+            erb :'games/index'
+        else
+            redirect '/signup'
+        end
+    end
