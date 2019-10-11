8924922b0ed193fb719da2704d01c531d8dfdf28
Nate <nprathe10@gmail.com>
2019-10-11 17:29:33 -0500

    added db

---
+++ db/migrate/20191011221925_create_games.rb
@@ -0,0 +1,10 @@
+class CreateGames < ActiveRecord::Migration
+  def change
+    create_table :games do |t|
+      t.string :title
+      t.string :genre
+      t.integer :rating
+      t.integer :user_id
+    end
+  end
+end
